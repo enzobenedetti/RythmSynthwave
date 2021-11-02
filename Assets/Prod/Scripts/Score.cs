f1f82d434fe3d7245c54f08326fe502145dcfb44
enzobenedetti <enzob210401@gmail.com>
2021-11-02 09:23:44 +0100

    Set UI Score in new Scripts

--- Assets/Prod/Scripts/Score.cs
+++ Assets/Prod/Scripts/Score.cs
@@ -1,67 +1,47 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class Score : MonoBehaviour
+public class Score
 {
-    public static Text scoreText;
-    public static Text comboText;
-    
-    private static int _score = 0;
 
-    private static int combo = 0;
+    public static int score = 0;
+
+    public static int Combo = 0;
     
     public static bool OnCombo;
-    
-    void Awake()
-    {
-        scoreText = GameObject.Find("Score").GetComponent<Text>();
-        comboText = GameObject.Find("Combo").GetComponent<Text>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    
-    }
-
-    static void UpdateUI()
-    {
-        scoreText.text = "Score : " + _score;
-        comboText.text = "Combo : " + combo;
-    }
 
     public static void AddCombo()
     {
-        combo++;
+        Combo++;
         OnCombo = true;
-        if (combo < 5) return;
-        if (combo < 15) _score += 1;
-        else if (combo < 30) _score += 5;
-        else _score += 10;
+        if (Combo < 5) return;
+        if (Combo < 15) score += 1;
+        else if (Combo < 30) score += 5;
+        else score += 10;
     }
 
     public static void GotPerfect()
     {
-        _score += 10;
+        score += 10;
         AddCombo();
-        UpdateUI();
+        ScoreUI.UpdateUI();
     }
     public static void GotNice()
     {
-        _score += 5;
+        score += 5;
         AddCombo();
-        UpdateUI();
+        ScoreUI.UpdateUI();
     }
     public static void GotOk()
     {
-        _score += 1;
+        score += 1;
         AddCombo();
-        UpdateUI();
+        ScoreUI.UpdateUI();
     }
 
     public static void GotBad()
     {
-        combo = 0;
-        UpdateUI();
+        Combo = 0;
+        ScoreUI.UpdateUI();
     }
 }
