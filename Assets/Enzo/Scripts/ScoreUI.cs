444d5c76beca3f7cee73d177e73d53c7ea636bdb
enzobenedetti <enzob210401@gmail.com>
2021-11-02 16:19:55 +0100

    howling - part2

--- Assets/Enzo/Scripts/ScoreUI.cs
+++ Assets/Enzo/Scripts/ScoreUI.cs
@@ -9,12 +9,18 @@ public class ScoreUI : MonoBehaviour
     public static Text scoreText;
     public static Text comboText;
     public static Text maxText;
+    public Text music;
+    public Text author;
+    public MusicPlayer playerMusic;
+    
     // Start is called before the first frame update
     void Awake()
     {
         scoreText = GameObject.Find("Score").GetComponent<Text>();
         comboText = GameObject.Find("Combo").GetComponent<Text>();
         maxText = GameObject.Find("MaxCombo").GetComponent<Text>();
+        music.text = playerMusic.track.name;
+        author.text = playerMusic.track.author;
     }
 
     // Update is called once per frame
