b154aee34f7e346fca16c88c51bcc5965623a1fb
enzobenedetti <enzob210401@gmail.com>
2021-11-22 16:15:34 +0100

    Use DoTween on touch

--- Assets/Enzo/Scripts/StartTouchBehavior.cs
+++ Assets/Enzo/Scripts/StartTouchBehavior.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class StartTouchBehavior : MonoBehaviour
 {
@@ -10,17 +11,21 @@ public class StartTouchBehavior : MonoBehaviour
         foreach (Transform number in transform)
         {
             number.gameObject.SetActive(true);
+            SpriteRenderer numberSprite = number.GetComponent<SpriteRenderer>();
+            numberSprite.color = new Color(1f, 1f, 1f, 0f);
+            numberSprite.DOFade(1f, 1f);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Timer.timer >= -1f)
+        if (Timer.timer >= -1.5f)
         {
             foreach (Transform number in transform)
             {
-                number.gameObject.SetActive(false);
+                SpriteRenderer numberSprite = number.GetComponent<SpriteRenderer>();
+                numberSprite.DOFade(0f, 1f);
             }
         }
     }
