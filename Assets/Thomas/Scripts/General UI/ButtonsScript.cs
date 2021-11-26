3b860d8588edc3034c5155e4b28b16cf79298027
enzobenedetti <enzob210401@gmail.com>
2021-11-26 14:18:34 +0100

    Adjut Pause and transition

--- Assets/Thomas/Scripts/General UI/ButtonsScript.cs
+++ Assets/Thomas/Scripts/General UI/ButtonsScript.cs
@@ -71,7 +71,7 @@ public class ButtonsScript : MonoBehaviour
 
     public static void GoToMainMenu()
     {
-        SceneManager.LoadSceneAsync("MenuScene");
+        SceneManager.LoadScene("MenuScene");
     }
 
 }
