85dd7c228dced4aa5205bd4edcbb6709ae67e551
enzobenedetti <enzob210401@gmail.com>
2021-11-29 15:37:27 +0100

    Fix Mouse bug
    This was a stupid one

---
+++ Assets/Enzo/CatchMouse.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CatchMouse : MonoBehaviour
+{
+    GameObject lastselect;
+    void Start()
+    {
+        lastselect = new GameObject();
+    }
+    // Update is called once per frame
+    void Update () {         
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(lastselect);
+        }
+        else
+        {
+            lastselect = EventSystem.current.currentSelectedGameObject;
+        }
+    }
+}
