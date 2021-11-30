3b5552fe69e57594b96a543358920a368e69d310
Thomas <thomas.hebert84@orange.fr>
2021-11-30 21:54:22 +0100

    Finished Save & load high scores

--- Assets/Prod/Scripts/EndLevelUI.cs
+++ Assets/Prod/Scripts/EndLevelUI.cs
@@ -60,7 +60,11 @@ public class EndLevelUI : MonoBehaviour
 
         if (Score.score > SaveData.LoadHighScore().highscores[Score.music.songId])
         {
-            
+            int[] MusicHsData = new int[3] {0, 0, 0};
+            if (!SaveData.HighScoresExist()) SaveData.SaveBadges(new[] {0, 0, 0});
+            else MusicHsData = SaveData.LoadHighScore().highscores;
+            MusicHsData[Score.music.songId] = Score.score;
+            SaveData.SaveHighScore(MusicHsData);
         }
     }
 
